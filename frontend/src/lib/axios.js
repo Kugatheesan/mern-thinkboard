920f4a4ffe54f5a0f9f8555be61e9da2a49de39c
Kugatheesan <git config --global user.email>
2025-06-24 23:07:07 +0530

    axios change

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // in production, there's no localhost so we have to make this dynamic
-const BASE_URL = "https://mern-thinkboard-e5qe.onrender.com/"
+const BASE_URL = "http://localhost:5001/api"
 const api = axios.create({
   baseURL: BASE_URL,
 });
